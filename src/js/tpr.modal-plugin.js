571c3010801d3ea7ec1836d01d2643648a4fa54c
Antonio Ángel de la Mata López <antonio.delamata@atmira.com>
2025-02-10 09:37:05 +0100

    feat: (migration) first try to es6

--- src/js/tpr.modal-plugin.js
+++ src/js/tpr.modal-plugin.js
@@ -1,339 +1,267 @@
-
 /**
-* 
-* Copyright (c) 2016 Antonio Ángel de la Mata López
-* Permission is hereby granted, free of charge, to any person obtaining a copy 
-* of this software and associated documentation files (the "Software"), to deal 
-* in the Software without restriction, including without limitation the rights to 
-* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-* the Software, and to permit persons to whom the Software is furnished to do so, 
-* subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all 
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-* DEALINGS IN THE SOFTWARE.
-*
-**/
-	(function (window, jQuery, undefined) {
+ *
+ * Copyright (c) 2016 Antonio Ángel de la Mata López
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ **/
+class TPRModal {
+  constructor(opts = {}) {
+    this.defaults = {
+      closable: true,
+      styleClass: "tpr-modalbox-grey",
+      backLayer: true,
+      closeOutClick: true,
+      moreInfo: {
+        state: false,
+        message: "",
+        buttonText: "More info",
+      },
+      buttons: {
+        accept: {
+          state: true,
+          text: "Accept",
+          class: "default",
+        },
+        cancel: {
+          state: true,
+          text: "Cancel",
+          class: "default",
+        },
+        close: {
+          state: true,
+          text: "Close",
+          class: "default-close",
+        },
+      },
+    };
 
-		
-		TPRMODAL = function (opts) {
+    this.options = { ...this.defaults, ...opts };
+    this.$modal = {
+      $modalBox: this._createElement("<div></div>"),
+      $modalBackground: this._createElement(
+        '<div class="tpr-modal-background"></div>'
+      ),
+      $modalTopBar: this._createElement('<div class="tpr-modal-topbar"></div>'),
+      $modalContent: this._createElement(
+        '<div class="tpr-modal-content"></div>'
+      ),
+      $modalBotBar: this._createElement('<div class="tpr-modal-botbar"></div>'),
+      $modalMoreBox: this._createElement(
+        '<div class="tpr-modal-morebox"></div>'
+      ),
+      $modalMoreLBtn: this._createElement(
+        '<a href="#" class="tpr-modal-morebutton"></a>'
+      ),
+      $closeButton: this._createElement(
+        '<a href="#" class="tpr-modal-close"></a>'
+      ),
+      $modalTitle: this._createElement("<p></p>"),
+      $modalMessage: this._createElement("<p></p>"),
+    };
 
-			this.defaults   = {
-				closable   : true,
-				styleclass : 'tpr-modalbox-grey',
-				backlayer  : true,
-				closeoutclick : true,
-				moreinfo   : {
-					state   	: false,
-					message 	: "",
-					buttontext  : "More info"
-				},
-				buttons    : {
-					accept : {
-						state   : true,
-						text    : 'Accept',
-						class   : 'default'
-					},
-					cancel : {
-						state   : true,
-						text    : 'Cancel',
-						class   : 'default'
-					},
-					close  : {
-						state   : true,
-						text    : 'Close',
-						class   : 'default-close'
-					}
-				}
-			};
+    this.title = opts.title || "Default title";
+    this.message = opts.message || "Default message";
+    this.messageExplain = opts.messageExplain || "Default message explain";
 
-			this.$modal  = {
-				$modalbox        : this._createElement('<div></div>'),
-				$modalbackground : this._createElement('<div class="tpr-modal-background"></div>'),
-				$modaltopbar     : this._createElement('<div class="tpr-modal-topbar"></div>'),
-				$modalcontent    : this._createElement('<div class="tpr-modal-content"></div>'),
-				$modalbotbar     : this._createElement('<div class="tpr-modal-botbar"></div>'),
-				$modalmorebox    : this._createElement('<div class="tpr-modal-morebox"></div>'),
-				$modalmorelbtn   : this._createElement('<a href="#" class="tpr-modal-morebutton"></a>'),
-				$closebutton     : this._createElement('<a href="#" class="tpr-modal-close" ></a>'),
-				$modaltitle      : this._createElement('<p></p>'),
-				$modalmessage    : this._createElement('<p></p>')
-			};
+    this.$body = $("body");
+    this._buildModal();
+    this._buildButtons();
+    this._attachEvent(this.$modal.$closeButton, "click", () => this.hide());
+  }
 
-			this.options = {};
-			$.extend(true, this.options ,this.defaults, opts);
+  _createElement(element) {
+    return $(element);
+  }
 
-			this.$el     = this.$modal.$modalbox;
-			this.$body   = jQuery('body');
+  _insertChilds(parent, childs) {
+    if (parent && childs) {
+      if (Array.isArray(childs)) {
+        childs.forEach((child) => parent.append(child));
+      } else {
+        parent.append(childs);
+      }
+    }
+  }
 
-			//
-			// Modal texts
-			this.title   		= (opts && opts.title)  		|| 'Default title';
-			this.message 		= (opts && opts.message) 		|| 'Default message';
-			this.messageExplain = (opts && opts.messageExplain) || 'Default message explain';
+  _buildButtons() {
+    this.$modal.$modalBotBar.empty();
+    Object.entries(this.options.buttons).forEach(([index, element]) => {
+      if (element !== false) {
+        const $button = this._createElement(
+          `<a href="#" class="tpr-modal-button ${element.class}">${element.text}</a>`
+        );
+        this._insertChilds(this.$modal.$modalBotBar, $button);
+        this._attachEvent($button, "click", index);
+      }
+    });
+  }
 
-			//
-			// Modal construction
-			this.constructor ();
-			
-		};
+  _attachEvent(element, event, executor) {
+    if (element && event && executor) {
+      element.off(event).on(event, (event) => {
+        event.stopPropagation();
+        event.preventDefault();
 
-		TPRMODAL.prototype = {
-			_insertChilds : function (parent, childs) {
-				var self = this;
-				if (parent && childs) {
-					if (childs.length) {
-						for (var i = 0; i < childs.length; i++) {
-							parent.append (childs[i]);
-						};
-					}else {
-						parent.append (childs);
-					}
-				}
-			},
-			_createElement : function (element) {
-				var self = this;
-				if (element) {
-					return jQuery(element);
-				}
-			},
-			_buildButtons : function () {
-				var self = this;
-				self.$modal.$modalbotbar.empty ();
+        if (typeof executor === "function") {
+          executor();
+        } else {
+          this.$modal.$modalBox.trigger(executor, [this, element]);
+        }
+      });
+    }
+  }
 
-				//
-				// Configure botbar
-				$.each (self.options.buttons, function (index, element) {
-					if (element !== false) {
-						var $button = self._createElement('<a href="#" class="tpr-modal-button ' + element.class + '">' + element.text + '</a>');
-						self._insertChilds(self.$modal.$modalbotbar, $button);
-						self._attachEvent ($button, 'click', index);
-					}
-				});
-			},
+  _buildModal() {
+    const { styleClass, moreInfo, backLayer } = this.options;
 
-			_attachEvent : function (element, event, executor) {
-				var self = this;
-				if (element && event && executor) {
-					if (!$.isFunction (executor)) {
-						element.off (event);
-						element.on (event, function (event) {
-							event.stopPropagation();
-							event.preventDefault();
+    this.$modal.$modalBox.addClass(styleClass);
 
-							self.$modal.$modalbox.trigger (executor, [self, element]);
-						});
-					}else {
-						element.off (event);
-						element.on (event, function (event) {
-							event.stopPropagation();
-							event.preventDefault();
+    // Top bar
+    this._insertChilds(this.$modal.$modalTopBar, [
+      this.$modal.$modalTitle.text(this.title),
+      this.$modal.$closeButton,
+    ]);
 
-							executor();
-						});
-					}
-					
-				}
-			},
+    // Content
+    this._insertChilds(
+      this.$modal.$modalContent,
+      this.$modal.$modalMessage.text(this.message)
+    );
 
-			_buildModal   : function () {
-				var self = this;
+    // More info
+    this.$modal.$modalMoreLBtn.text(moreInfo.buttonText);
+    this._insertChilds(this.$modal.$modalMoreBox, moreInfo.message);
+    this._insertChilds(this.$modal.$modalContent, [
+      this.$modal.$modalMoreLBtn,
+      this.$modal.$modalMoreBox,
+    ]);
 
-				//
-				// Set parent class that determine
-				// style of the entire modal
-				self.$modal.$modalbox
-					.addClass (self.options.styleclass);
+    if (moreInfo.state) {
+      this.$modal.$modalMoreLBtn.css({ display: "inline-block" });
+      this._attachEvent(this.$modal.$modalMoreLBtn, "click", () => {
+        this.$modal.$modalMoreBox.slideToggle();
+      });
+    }
 
+    this._insertChilds(this.$modal.$modalBox, [
+      this.$modal.$modalTopBar,
+      this.$modal.$modalContent,
+      this.$modal.$modalBotBar,
+    ]);
 
-				//
-				// Configure topbar
-				self._insertChilds(self.$modal.$modaltopbar,
-									[
-									 self.$modal.$modaltitle.text (self.title),
-									 self.$modal.$closebutton
-									]);
-		
-				//
-				// Configure content
-				self._insertChilds(self.$modal.$modalcontent, self.$modal.$modalmessage.text (self.message));
+    if (backLayer) {
+      this.$modal.$modalBackground.appendTo(this.$body);
+      this.$modal.$modalBox.appendTo(this.$modal.$modalBackground);
 
-				//
-				// Has more info box?
-				self.$modal.$modalmorelbtn.text (self.options.moreinfo.buttontext);
-				self._insertChilds(self.$modal.$modalmorebox, self.options.moreinfo.message);
+      if (this.options.closeOutClick) {
+        this._attachEvent(this.$modal.$modalBackground, "click", () =>
+          this.hide()
+        );
+      }
+    } else {
+      this.$modal.$modalBox.appendTo(this.$body);
+    }
 
-				self._insertChilds(self.$modal.$modalcontent, [
-					self.$modal.$modalmorelbtn,
-					self.$modal.$modalmorebox
-				]);
+    if (!this.options.closable) {
+      this.setClosable(false);
+    }
 
-				if (self.options.moreinfo.state === true) {
-					self.$modal.$modalmorelbtn.css({"display" : "inline-block"});
-					self._attachEvent (self.$modal.$modalmorelbtn, 'click', function () {
-						self.$modal.$modalmorebox.slideToggle();
-					});
-				}
+    this._centerModal();
+  }
 
-				self._insertChilds(self.$modal.$modalbox, [
-					self.$modal.$modaltopbar,
-					self.$modal.$modalcontent,
-					self.$modal.$modalbotbar
-				]);
+  _centerModal() {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const modalWidth = Math.floor(
+      (this.$modal.$modalBox.innerWidth() / 100) * windowWidth
+    );
+    const modalHeight =
+      Math.floor((this.$modal.$modalBox.innerHeight() / 100) * windowHeight) +
+      400;
 
-				// Has background?
-				if (self.options.backlayer === true) {
-					self.$modal.$modalbackground.appendTo (self.$body);
-					self.$modal.$modalbox.appendTo (self.$modal.$modalbackground);
+    const xPosition = windowWidth / 2 - modalWidth / 2;
+    const yPosition = windowHeight / 2 - modalHeight / 2;
 
-					//
-					// When click on background close?
-					if (self.options.closeoutclick) {
-						self._attachEvent (self.$modal.$modalbackground, 'click', function () {
-							self.hide ();
-						});
-					}
-				} else {
-					self.$modal.$modalbox.appendTo (self.$body);
-				}
+    this.$modal.$modalBox.css({
+      left: `${xPosition}px`,
+      top: `${yPosition}px`,
+    });
 
-				//
-				// Is cosable?
-				if (!self.options.closable) {
-					self.setClosable (false);
-				}
+    // Prevent propagation
+    this._attachEvent(this.$modal.$modalBox, "click", () => false);
+  }
 
+  setTitle(title) {
+    this.$modal.$modalTitle.text(title);
+  }
 
-				//
-				// CENTER MODAL
-				var windowWidth  = window.innerWidth; 
-				var windowHeight = window.innerHeight;
-				var modalWidth   = Math.floor ((self.$modal.$modalbox.innerWidth() / 100) * windowWidth);
-				var modalHeight  = Math.floor ((self.$modal.$modalbox.innerHeight() / 100) * windowHeight) + 400;
-				
-				var xPosition    = (windowWidth / 2) - (modalWidth / 2);
-				var yPosition    = (windowHeight / 2) - (modalHeight / 2);
+  setMessage(message) {
+    this.$modal.$modalMessage.text(message);
+  }
 
-				//
-				// SET x, y POSITIONS
-				self.$modal.$modalbox.css ({
-					"left" : xPosition + 'px',
-					"top"  : yPosition + 'px'
-				});
+  setMessageExplain(message) {
+    this.$modal.$modalMoreBox.text(message);
+  }
 
-				//
-				// This event stop propagation from inner modalbox tags
-				self._attachEvent (self.$modal.$modalbox, 'click', function () {
-					return false;
-				});
+  setMoreInfoState(state) {
+    if (state) {
+      this.$modal.$modalMoreLBtn.css({ display: "inline-block" });
+      this._attachEvent(this.$modal.$modalMoreLBtn, "click", () => {
+        this.$modal.$modalMoreBox.slideToggle();
+      });
+    } else {
+      this.$modal.$modalMoreLBtn.hide();
+    }
+  }
 
-				self.$el = self.$modal.$modalmorebox;
+  setStyle(styleClass) {
+    this.$modal.$modalBox.removeClass().addClass(styleClass);
+  }
 
-			},
-			on : function (event, callback) {
-				var self = this;
-				self.$modal.$modalbox.off (event);
-				self.$modal.$modalbox.on (event, callback);
-			},			
+  setClosable(closable) {
+    if (closable) {
+      this.$modal.$closeButton.show();
+      this._attachEvent(this.$modal.$modalBackground, "click", () =>
+        this.$modal.$modalBackground.hide()
+      );
+    } else {
+      this.$modal.$closeButton.hide();
+      this.$modal.$modalBackground.off("click");
+    }
+  }
 
-			//
-			// Configure modal text
-			setTitle   		  : function (title) {
-				var self = this;
-				self.$modal.$modaltitle.text (title);
-			},
-			setMessage 		  : function (message) {
-				var self = this;
-				self.$modal.$modalmessage.text (message);
-			},
-			setMessageExplain : function (msg) {
-				var self = this;
-				self.$modal.$modalmorebox.text (msg);
-			},
-			setMoreinfoState  : function (state) {
-				var self = this;
-				if (state) {
-					self.$modal.$modalmorelbtn.css({"display" : "inline-block"});
-					self._attachEvent (self.$modal.$modalmorelbtn, 'click', function () {
-						self.$modal.$modalmorebox.slideToggle();
-					});
+  setDefaultButtonsHide() {
+    Object.values(this.options.buttons).forEach(
+      (button) => (button.state = false)
+    );
+  }
 
-				} else {
-					self.$modal.$modalmorelbtn.hide();
-				}
+  show() {
+    this.$modal.$modalBackground.show();
+  }
 
-			},
-			setMoreinfoButtonText : function (text) {
-				var self = this;
-				if (text) {
-					self.$modal.$modalmorelbtn.text (text);
-				}
-			},
-			setStyle          : function (styleClass) {
-				var self   = this;
-				var cClass = self.$modal.$modalbox.attr ('class');
-				self.$modal.$modalbox
-					.addClass (styleClass)
-					.removeClass (cClass);
-			},
+  hide() {
+    this.$modal.$modalBackground.hide();
+    this.$modal.$modalMoreBox.hide();
+  }
 
-			setClosable : function (closable) {
-				var self = this;
-				if (closable === true) {
-					self.$modal.$closebutton.show ();
-					self._attachEvent (self.$modal.$modalbackground, 'click', function () {
-						self.$modal.$modalbackground.hide ();
-					});
-				}else {
-					self.$modal.$closebutton.hide ();
-					self.$modal.$modalbackground.off ('click');
-				}
-			},
-
-			setDefaultButtonsHide : function () {
-				var self = this;
-				$.each (self.options.buttons, function (index, element) {
-					element.state = false;
-				});
-			},
-
-			//
-			// Visibility control
-			show : function () {
-				var self = this;
-				self.$modal.$modalbackground.show ();
-
-			},
-			hide : function () {
-				var self = this;
-				self.$modal.$modalbackground.hide ();
-				self.$modal.$modalmorebox.hide ();
-			},
-
-			setButtons : function (buttons) {
-				var self = this;  
-				 $.extend (true, self.options.buttons, buttons);
-				 self._buildButtons ();
-			},
-
-			constructor : function () {
-				var self = this;
-
-				// First step, build DOM element
-				self._buildModal ();
-				self._buildButtons ();
-				self._attachEvent (self.$modal.$closebutton, 'click', function () {
-					self.hide ();
-				});
-			}
-		};
-
-
-	})(window, $);
+  setButtons(buttons) {
+    this.options.buttons = { ...this.options.buttons, ...buttons };
+    this._buildButtons();
+  }
+}
